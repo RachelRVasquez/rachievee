3663587621d5473369162a442ecdcb2e5774a037
Rachel Vasquez <rachelvasquez@Rachels-MBP.home>
2020-02-15 14:09:43 -0500

    new js/sass structure

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//import other stylesheets here
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    console.log('testing Js');
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
