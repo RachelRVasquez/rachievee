048562a1bd2769924c32075238f33cf642e20eb7
Rachel Vasquez <rachelvasquez@Rachels-MBP.home>
2020-03-08 01:09:54 -0500

    new page template for portfolio, portfolio post type/taxonomy, working on styling

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,87 @@ function rachievee_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'rachievee_pingback_header' );
+
+function register_portfolio_post_type() {
+	$labels = array(
+		'name'                  => _x( 'Portfolio', 'Post Type General Name', 'rachievee' ),
+		'singular_name'         => _x( 'Portfolio', 'Post Type Singular Name', 'rachievee' ),
+		'menu_name'             => __( 'Portfolio', 'rachievee' ),
+		'name_admin_bar'        => __( 'Portfolio', 'rachievee' ),
+		'archives'              => __( 'Portolio Archives', 'rachievee' ),
+		'attributes'            => __( 'Portfolio Attributes', 'rachievee' ),
+		'all_items'             => __( 'All Portfolio Features', 'rachievee' ),
+		'add_new_item'          => __( 'Add New Portfolio Feature', 'rachievee' ),
+		'add_new'               => __( 'Add New Feature', 'rachievee' ),
+		'new_item'              => __( 'New Feature', 'rachievee' ),
+		'edit_item'             => __( 'Edit Portfolio Feature', 'rachievee' ),
+		'update_item'           => __( 'Update Feature', 'rachievee' ),
+		'view_item'             => __( 'View Feature', 'rachievee' ),
+		'view_items'            => __( 'View Features', 'rachievee' ),
+		'search_items'          => __( 'Search Portfolio', 'rachievee' ),
+		'featured_image'        => __( 'Portfolio Image', 'rachievee' ),
+		'set_featured_image'    => __( 'Set portfolio image', 'rachievee' ),
+		'remove_featured_image' => __( 'Remove portfolio image', 'rachievee' ),
+		'use_featured_image'    => __( 'Use as portfolio image', 'rachievee' ),
+		'filter_items_list'     => __( 'Filter items list', 'rachievee' ),
+	);
+	$args = array(
+		'label'                 => __( 'Portfolio', 'rachievee' ),
+		'description'           => __( 'Portfolio Features', 'rachievee' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions' ),
+		'hierarchical'          => false,
+		'public'                => false,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-businesswoman',
+		'show_in_admin_bar'     => false,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => true,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+
+	register_post_type( 'portfolio', $args );
+}
+add_action( 'init', 'register_portfolio_post_type', 0 );
+
+function register_portfolio_categories() {
+	$labels = array(
+		'name'                       => _x( 'Technologies', 'Taxonomy General Name', 'rachievee' ),
+		'singular_name'              => _x( 'Technology', 'Taxonomy Singular Name', 'rachievee' ),
+		'menu_name'                  => __( 'Technologies', 'rachievee' ),
+		'all_items'                  => __( 'All Items', 'rachievee' ),
+		'parent_item'                => __( 'Parent Item', 'rachievee' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'rachievee' ),
+		'new_item_name'              => __( 'New Item Name', 'rachievee' ),
+		'add_new_item'               => __( 'Add New Item', 'rachievee' ),
+		'edit_item'                  => __( 'Edit Item', 'rachievee' ),
+		'update_item'                => __( 'Update Item', 'rachievee' ),
+		'view_item'                  => __( 'View Item', 'rachievee' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'rachievee' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'rachievee' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'rachievee' ),
+		'popular_items'              => __( 'Popular Items', 'rachievee' ),
+		'search_items'               => __( 'Search Items', 'rachievee' ),
+		'not_found'                  => __( 'Not Found', 'rachievee' ),
+		'no_terms'                   => __( 'No items', 'rachievee' ),
+		'items_list'                 => __( 'Items list', 'rachievee' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'rachievee' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+
+	register_taxonomy( 'technologies', array( 'portfolio' ), $args );
+}
+add_action( 'init', 'register_portfolio_categories', 0 );
---
+++ page-portfolio.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Template Name: Portfolio
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package rachievee
+ */
+
+get_header();
+get_sidebar();
+
+$get_portfolio_posts = get_posts([
+	'post_type' => 'portfolio',
+	'orderby'   => 'date',
+	'order'     => 'DESC',
+]); ?>
+
+	<div id="primary" class="content-area portfolio-content">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+		if ( $get_portfolio_posts && count( $get_portfolio_posts ) > 0 ) {
+			foreach ( $get_portfolio_posts as $portfolio ) {
+				$port_image = get_the_post_thumbnail( $portfolio->ID );
+				$port_post  = get_post( $portfolio->ID );
+				$get_technologies = get_the_terms( $portfolio->ID, 'technologies' );
+				$tech_list = $get_technologies ? join(', ', wp_list_pluck( $get_technologies, 'name' ) ) : '';
+				?>
+				<section class="port-section">
+					<?php if ( $port_image ) { ?>
+						<div class="port-img-wrapper">
+							<?php echo $port_image; ?>
+						</div>
+					<?php } ?>
+					<div class="port-content">
+						<h3 class="port-title"><?php echo esc_html( get_the_title( $portfolio->ID ) ); ?></h3>
+						<p class="port-excerpt"><?php echo get_the_excerpt( $portfolio->ID ); ?></p>
+						<?php if ( ! empty( $tech_list ) ) { ?>
+							<p class="port-tech-list">
+								<?php echo $tech_list; ?>
+							</p>
+						<?php } ?>
+						<button class="port-more-btn" name="read-more-portfolio">
+							<?php esc_html_e( 'More about this project', 'rachievee' ); ?>
+							<span aria-hidden="true">&#9660;</span>
+						</button>
+						<div class="port-full">
+							<?php if ( isset( $port_post ) && !empty( $port_post ) ) {
+								echo wpautop( $port_post->post_content );
+							} ?>
+						</div>
+					</div>
+				</section>
+			<?php }
+		}
+
+		// get_template_part( 'template-parts/content', 'page' );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
